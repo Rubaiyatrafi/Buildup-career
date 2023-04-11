673ea73cf70cc8ab1de46bdef2355bb0272e337a
Rubaiyatrafi <rubaiyatkabirrafi9166@gmail.com>
2023-04-11 15:58:06 +0600

    Job List data displayed

--- src/Components/BodyThird/BodyThird.jsx
+++ src/Components/BodyThird/BodyThird.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Jobs from "../Jobs/Jobs";
+import "./BodyThird.css";
 
 const BodyThird = () => {
   const [jobsList, setJobsList] = useState([]);
@@ -9,10 +10,19 @@ const BodyThird = () => {
       .then((data) => setJobsList(data));
   }, []);
   return (
-    <div>
-      {jobsList.map((jobs) => (
-        <Jobs key={jobs.id} jobs={jobs}></Jobs>
-      ))}
+    <div className="body2">
+      <div className="bodyDetails">
+        <h1 className="title text1">Featured Jobs</h1>
+        <p className="details">
+          Explore thousands of job opportunities with all the information you
+          need. Its your future
+        </p>
+      </div>
+      <div className="jobs-list">
+        {jobsList.map((jobs) => (
+          <Jobs key={jobs.id} jobs={jobs}></Jobs>
+        ))}
+      </div>
     </div>
   );
 };
--- src/Components/Jobs/Jobs.jsx
+++ src/Components/Jobs/Jobs.jsx
@@ -1,11 +1,45 @@
 import React from "react";
 import "./Jobs.css";
+import img1 from "../../../public/images/Frame-4.png";
+import img2 from "../../../public/images/Frame.png";
 
 const Jobs = ({ jobs }) => {
-  const { id, logo } = jobs;
+  const {
+    id,
+    logo,
+    company_name,
+    post_name,
+    email,
+    address,
+    job_responsibilities,
+    job_description,
+    educational_requirements,
+    experience,
+    work_details,
+    salary,
+  } = jobs;
   return (
-    <div>
-      <img src={logo} alt="" />
+    <div className="job-container">
+      <div>
+        <img className="jobs-img" src={logo} alt="" />
+        <h4 className="post-name">{post_name}</h4>
+        <h6 className="com-name">{company_name}</h6>
+        <div className="work-container">
+          <p className="work-details">{work_details.schedule1}</p>
+          <p className="work-details">{work_details.schedule3}</p>
+        </div>
+        <div className="work-container">
+          <div className="work-container">
+            <img className="location-img" src={img1} alt="" />
+            <p className="add-details">{address}</p>
+          </div>
+          <div className="work-container">
+            <img className="location-img" src={img2} alt="" />
+            <p className="add-details">{salary}</p>
+          </div>
+        </div>
+        <button className="btn-view">View Details</button>
+      </div>
     </div>
   );
 };
